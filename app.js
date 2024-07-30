465cf82adaf75569909a3d47192d018be4cb7589
Shahzad090 <153703186+Shahzad090@users.noreply.github.com>
2024-07-31 02:01:25 +0500

    continue foam coming

--- app.js
+++ app.js
@@ -21,3 +21,20 @@
         const app = initializeApp(firebaseConfig);
         const analytics = getAnalytics(app);
       
+
+        import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  // ...
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
